297c9a7fa1736c2c452ca8c301a3c33dcf88f7bb
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2018-05-23 17:29:15 +0200

    initial version

---
+++ start-services.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# when using VOLUME in Dockerfile, /var/lib/postgresql gets owned by user of the host system
+chown -R postgres:postgres /var/lib/postgresql
+service postgresql start
+python3 /opt/netbox/netbox/manage.py runserver 0.0.0.0:8000 --insecure &
+bash
