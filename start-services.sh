5d8e9bebc1d56cfaa0038e7a6439e9e4c9ea5d1d
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2018-05-23 22:10:24 +0200

    improved startup file

--- start-services.sh
+++ start-services.sh
@@ -2,6 +2,10 @@
 
 # when using VOLUME in Dockerfile, /var/lib/postgresql gets owned by user of the host system
 chown -R postgres:postgres /var/lib/postgresql
+sleep 1
+
 service postgresql start
+
 python3 /opt/netbox/netbox/manage.py runserver 0.0.0.0:8000 --insecure &
+
 bash
